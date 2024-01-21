54c9a31c312dc615052cda9c63693f00900be602
BuildTools <unconfigured@null.spigotmc.org>
2024-01-21 12:00:35 -0500

    Add project files.

---
+++ SplotchModTemplate.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace SplotchModTemplate
+{
+    public class SplotchModTemplate : Splotch.SplotchMod
+    {
+        public override void OnLoad()
+        {
+            Debug.Log("Hello Bopl Battle!");
+            harmony.PatchAll(typeof(BasicPatch));
+        }
+    }
+
+    [HarmonyPatch(typeof(Ability))]
+    public class BasicPatch
+    {
+        [HarmonyPatch(nameof(Ability.EnterAbility))]
+        [HarmonyPrefix]
+        public void AbilityEnter()
+        {
+            Debug.Log("Entered ability!");
+        }
+
+        [HarmonyPatch(nameof(Ability.ExitAbility))]
+        [HarmonyPrefix]
+        public void AbilityExit()
+        {
+            Debug.Log("Exited ability!");
+        }
+    }
+}
